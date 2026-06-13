809fc3622aeb15ea2f088e55fc6ba9a5fc3109a6
Dev Ramudit <dramudit@gmail.com>
2026-06-13 10:58:59 -0400

    Added player animation

--- main_scene.py
+++ main_scene.py
@@ -34,6 +34,9 @@ DOWN = "down"
 UP = "up"
 LEFT = "left"
 RIGHT = "right"
+IDLE = "idle"
+WALK_1 = "walk_1"  
+WALK_2 = "walk_2"
 PLAYER_SPRITESHEET_SIZE = 36
 
 class MainScene(Scene):
@@ -45,8 +48,11 @@ class MainScene(Scene):
         self.player_spritesheet_img = pygame.image.load("img/GroundHog.png").convert_alpha()
         self.player_sprites = defaultdict(dict)
         self.last_player_direction = DOWN
+        self.player_position = IDLE
+        self.player_walk_cycle_secs = 0.3
+        self.player_time_moving = 0
         y_names = {0: DOWN, 1: LEFT, 2: RIGHT, 3: UP}
-        x_names = {0: "idle", 1: "walk_1", 2: "walk_2"}
+        x_names = {0: IDLE, 1: WALK_1, 2: WALK_2}
         for y in range(4):
             for x in range(3):
                 self.player_sprites[y_names[y]][x_names[x]] = self.player_spritesheet_img.subsurface(pygame.Rect(PLAYER_SPRITESHEET_SIZE*x, PLAYER_SPRITESHEET_SIZE*y, PLAYER_SPRITESHEET_SIZE, PLAYER_SPRITESHEET_SIZE))
@@ -66,26 +72,42 @@ class MainScene(Scene):
         screen.blit(timer_text, (10, 50))
         
         keys = pygame.key.get_pressed()
+        moving = False
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             player_rect.y -= 300 * dt
+            moving = True
             self.last_player_direction = UP
             if collidelist := player_rect.collidelistall(wall_rects):
                 player_rect.top = wall_rects[collidelist[0]].bottom
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
             player_rect.y += 300 * dt
+            moving = True
             self.last_player_direction = DOWN
             if collidelist := player_rect.collidelistall(wall_rects):
                 player_rect.bottom = wall_rects[collidelist[0]].top
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
             player_rect.x -= 300 * dt
+            moving = True
             self.last_player_direction = LEFT
             if collidelist := player_rect.collidelistall(wall_rects):
                 player_rect.left = wall_rects[collidelist[0]].right
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             player_rect.x += 300 * dt
+            moving = True
             self.last_player_direction = RIGHT
             if collidelist := player_rect.collidelistall(wall_rects):
                 player_rect.right = wall_rects[collidelist[0]].left
+        else:
+            self.player_time_moving = 0
+            self.player_position = IDLE
+
+        if moving:
+            self.player_time_moving += dt
+            if self.player_time_moving % self.player_walk_cycle_secs < self.player_walk_cycle_secs / 2:
+                self.player_position = WALK_1
+            else:
+                self.player_position = WALK_2
+
         if keys[pygame.K_ESCAPE]:
             return None
         if keys[pygame.K_SPACE]:
@@ -93,6 +115,6 @@ class MainScene(Scene):
 
         for tile in wall_rects:
             pygame.draw.rect(screen, (100, 100, 100), tile)
-        screen.blit(self.player_sprites[self.last_player_direction]["idle"], player_rect)
+        screen.blit(self.player_sprites[self.last_player_direction][self.player_position], player_rect)
 
         return self
