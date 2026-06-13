c0886b76064b4bf0675e813688ca693f460d33d7
Dev Ramudit <dramudit@gmail.com>
2026-06-12 20:19:56 -0400

    Removed bad gitignore and fixed title

--- screen.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Screen():
-    def __init__(self, screen, fill="black"):
-        self.screen = screen
-        self.fill = fill
-
-    def render(self):
-        self.screen.fill(self.fill)
